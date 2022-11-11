ae30036e9ac5fc890f9c883e8dda9f4c7dfe2ebf
Blake <blakepetersont@gmail.com>
2022-11-11 14:59:18 -0700

    added function to check time and to save data

--- index.js
+++ index.js
@@ -32,5 +32,38 @@ var hours = [
                  <button class="col-2 saveBtn"> Save Task</button>
                </div>;`
         }
+    } else{
+        var storage = []
+        for (let i =0; i < hours.length; i++) {
+            containerElement[0].innerHTML += `<div class="row">
+            <h3 class="col-2 time-block hour">
+            ${hours[i]}:00
+            </h3>
+            <textarea class="col-2 saveBtn"> Save Task</button>
+            </div>`;
+        }
+        localStorage.setItem("taks", JSON.stringify(storage))
     }
+  }
+
+  function checkClass(time) {
+    var now = moment().hours();
+
+    if (time < now) {
+        return "past";
+    }
+
+    if (time == now) {
+        return "present";
+    }
+
+    return "future";
+  }
+  window.onload = populateTable();
+  for (let i = 0; i < saveBtns.length; i++) {
+    saveBtns[i].addEventsListener("click", function (e){
+        console.log(e.target)
+        let textArea = document.getElementById(String(i))
+        storage = JSON.parse(localStorage.getItem("tasks"))
+    });
   }
